4f70f963b693ef1fb252400110612f08f83c1f4e
James Lu <james@overdrivenetworks.com>
2020-04-04 23:38:35 -0700

    Use QStandardPaths to locate mimeapps.list
    This saves us an extra dependency.

--- appsel.py
+++ appsel.py
@@ -6,9 +6,7 @@ import sys
 
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.uic import loadUi
-#from PyQt5.QtCore import *
-
-import xdg.BaseDirectory
+from PyQt5.QtCore import QStandardPaths
 
 SECTION_DEFAULTS = "Default Applications"
 SECTION_ADDED    = "Added Associations"
@@ -22,6 +20,7 @@ class AppSelector(QMainWindow):
         self._app = app
         self._ui = loadUi(uifile, self)
         self._ui.show()
+        print("mimeapps.list paths:", self._get_mimeapps_list_paths())
 
     @staticmethod
     def _get_mimeapps_list_paths():
@@ -31,14 +30,19 @@ class AppSelector(QMainWindow):
         Based off of: https://specifications.freedesktop.org/mime-apps-spec/latest/ar01s02.html
         """
         current_desktops = os.environ.get('XDG_CURRENT_DESKTOP', '').split(':')
+
         user_defaults_per_desktop = list(itertools.chain.from_iterable(
-            xdg.BaseDirectory.load_config_paths(f"mimeapps-{desktop}.list") for desktop in current_desktops
+            QStandardPaths.locateAll(QStandardPaths.ConfigLocation, f"mimeapps-{desktop}.list")
+            for desktop in current_desktops
         ))
-        user_defaults = list(xdg.BaseDirectory.load_config_paths("mimeapps.list"))
+        user_defaults = QStandardPaths.locateAll(QStandardPaths.ConfigLocation, "mimeapps.list")
+
         global_defaults_per_desktop = list(itertools.chain.from_iterable(
-            xdg.BaseDirectory.load_config_paths(f"mimeapps-{desktop}.list") for desktop in current_desktops
+            QStandardPaths.locateAll(QStandardPaths.ApplicationsLocation,
+                                     f"mimeapps-{desktop}.list")
+            for desktop in current_desktops
         ))
-        global_defaults = list(xdg.BaseDirectory.load_data_paths("applications/mimeapps.list"))
+        global_defaults = QStandardPaths.locateAll(QStandardPaths.ApplicationsLocation, "mimeapps.list")
         return user_defaults_per_desktop + user_defaults + global_defaults_per_desktop + global_defaults
 
 def main():
