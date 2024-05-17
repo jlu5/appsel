8a48209ce81811032844f9c461d9b0558ffd9a5e
James Lu <james@overdrivenetworks.com>
2024-05-16 19:29:17 -0700

    DefaultAppOptionsDelegate: override initStyleOption instead of paint
    This should fix a sporadic crash.

--- appsel/itemdelegates.py
+++ appsel/itemdelegates.py
@@ -5,21 +5,23 @@ from PyQt5.QtWidgets import QStyledItemDelegate
 
 class DefaultAppOptionsDelegate(QStyledItemDelegate):
     """
-    Provides custom styling for the default app chooser list view."""
+    Provides custom styling for the default app chooser list view.
+    """
     def __init__(self, model, attr):
         super().__init__()
         self.model = model
         self.attr = attr
 
-    def paint(self, painter, option, index):
-        """Paint handler:
+    def initStyleOption(self, option, index):
+        """
+        Override styling options for applications in SelectDefaultAppDialog:
+
         - Strike out disabled apps
         - Italicize custom associations
         - Bold the current default app
         """
+        super().initStyleOption(option, index)
         _app_id, options = getattr(self.model, self.attr)[index.row()]
         option.font.setStrikeOut(options.disabled)
         option.font.setItalic(options.custom)
         option.font.setBold(options.default)
-
-        return super().paint(painter, option, index)
