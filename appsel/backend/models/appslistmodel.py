81f89e61f2bd55cb6a983b54d09288f5e2d7ca93
James Lu <james@overdrivenetworks.com>
2024-05-22 14:30:37 +0800

    Apps view: show number of supported and default file types

--- appsel/backend/models/appslistmodel.py
+++ appsel/backend/models/appslistmodel.py
@@ -1,14 +1,16 @@
 
 # pylint: disable=invalid-name
-from PyQt5.QtCore import Qt, QAbstractListModel, QModelIndex, QVariant
+from PyQt5.QtCore import Qt, QAbstractTableModel, QModelIndex, QVariant
 
-class AppsListModel(QAbstractListModel):
+class AppsListModel(QAbstractTableModel):
     """
     Enumerates a list of applications (.desktop entries)
     """
-    def __init__(self, desktop_entries):
+    COLUMNS = ["Application", "# Supported File Types", "# Defaults"]
+    def __init__(self, manager):
         super().__init__()
-        self.desktop_entries = desktop_entries
+        self.manager = manager
+        self.desktop_entries = manager.desktop_entries
         self.refresh(first_run=True)
         self.sort(0)
 
@@ -17,13 +19,34 @@ class AppsListModel(QAbstractListModel):
         if not first_run:
             self.dataChanged.emit(QModelIndex(), QModelIndex())
 
-    def data(self, index, role):
+    def _get_app_name(self, index, role):
         app_id = self.apps[index.row()]
         if role == Qt.DisplayRole:  # Display text
             return self.desktop_entries.get_name(app_id)
         if role == Qt.DecorationRole:  # App icon
             return self.desktop_entries.get_icon(app_id)
         return QVariant()
+
+    def _get_num_supported_types(self, index, role):
+        app_id = self.apps[index.row()]
+        if role == Qt.DisplayRole:  # Display text
+            return len(self.manager.get_supported_types(app_id))
+        return QVariant()
+
+    def _get_num_defaults(self, index, role):
+        app_id = self.apps[index.row()]
+        if role == Qt.DisplayRole:  # Display text
+            supported_types = self.manager.get_supported_types(app_id)
+            return len([options for options in supported_types.values()
+                        if options.default])
+        return QVariant()
+
+    def data(self, index, role):
+        accessors = [self._get_app_name, self._get_num_supported_types, self._get_num_defaults]
+        try:
+            return accessors[index.column()](index, role)
+        except IndexError:
+            return QVariant()
 
     def sort(self, _column, order=Qt.AscendingOrder):
         """Sorts the model by the given column and order."""
@@ -36,8 +59,13 @@ class AppsListModel(QAbstractListModel):
         """
         return len(self.apps)
 
+    def headerData(self, section, orientation, role):
+        if role == Qt.DisplayRole and orientation == Qt.Horizontal:
+            return self.COLUMNS[section]
+        return QVariant()
+
     def columnCount(self, _index):
         """
         Return list of columns in the model.
         """
-        return 1
+        return len(self.COLUMNS)
